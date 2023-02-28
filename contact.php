1edc0e21118673fcdb36fbb963711a53bca9e2ac
Mark Karels <mak8966@uncw.edu>
2023-02-28 10:14:24 -0500

    Contact Form Work
    Started working a little bit on the gallery page

--- contact.php
+++ contact.php
@@ -1,14 +1,12 @@
-<!-- Mark Karels -->
-
 <?php
 $title = "Contact";
 require("header.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $message = $_POST["message"];
+    $fullname = filter_var($_POST["fullname"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $phone = filter_var($_POST["phone"], FILTER_SANITIZE_NUMBER_INT);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
     $errors = array();
 
@@ -21,17 +19,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         return true;
     }
 
-    validateInput($name, "name");
+    validateInput($fullname, "fullname");
     validateInput($email, "email");
     validateInput($phone, "phone");
     validateInput($message, "message");
+
+    // validate email address format using regex
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format.";
+    }
 
     if (!empty($errors)) {
         foreach ($errors as $error) {
             echo "<p>" . $error . "</p>";
         }
     } else {
-        echo "The form data has been processed!";
+        // send email
+        $to = "mak8966@uncw.edu";
+        $subject = "Contact Form Submission";
+        $body = "Name: $fullname\nEmail: $email\nPhone: $phone\nMessage: " . htmlspecialchars_decode($message);
+        $headers = "From: $email";
+
+        if (mail($to, $subject, $body, $headers)) {
+            echo "<p class='message' style='text-align: center;'>Thank you for contacting us. We will get back to you soon.</p>";
+        } else {
+            echo "<p class='message' style='text-align: center;'>There was an error sending your message. Please try again later.</p>";
+        }
     }
 }
 ?>
@@ -39,9 +52,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <section>
     <h1>Contact</h1>
     <p>Fill out the form below to get in touch with me.</p>
-    <form class="contact-form" action="submit-form.php" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
+    <form class="contact-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <label for="fullname">Name:</label>
+        <input type="text" id="fullname" name="fullname" required>
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
         <label for="phone">Phone Number:</label>
--- gallery.php
+++ gallery.php
@@ -3,14 +3,88 @@
 <?php
 $title = "Gallery";
 require("header.php");
+
+// set the directory where uploaded images will be saved
+$targetDir = "assets/images";
+
+// check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // get the uploaded file and other form data
+    $fileName = $_FILES["file"]["name"];
+    $fileType = $_FILES["file"]["type"];
+    $fileSize = $_FILES["file"]["size"];
+    $fileTmpName = $_FILES["file"]["tmp_name"];
+    $description = $_POST["description"];
+
+    // set the file path to save the uploaded image
+    $filePath = $targetDir . $fileName;
+
+    // check if the uploaded file is an image
+    if (getimagesize($fileTmpName)) {
+        // move the uploaded file to the target directory
+        if (move_uploaded_file($fileTmpName, $filePath)) {
+            // add the image data to a JSON file
+            $jsonData = file_get_contents("gallery.json");
+            $images = json_decode($jsonData, true);
+            $images[] = array("file" => $fileName, "description" => $description);
+            $jsonData = json_encode($images);
+            file_put_contents("gallery.json", $jsonData);
+
+            echo "<p>File uploaded successfully.</p>";
+        } else {
+            echo "<p>There was an error uploading your file. Please try again later.</p>";
+        }
+    } else {
+        echo "<p>Invalid file type. Please upload an image file.</p>";
+    }
+}
 ?>
 
 <section>
     <h1>Gallery</h1>
     <p>Show screenshots and/or videos of your working assignments to prospective employers.</p>
-    <!-- add screenshots and/or videos here -->
+
+    <!-- display images from JSON file -->
+    <?php
+    $jsonData = file_get_contents("gallery.json");
+    $images = json_decode($jsonData, true);
+
+    foreach ($images as $image) {
+        // create a div to contain the image and description
+        echo "<div class='image-container'>";
+
+        // display the image
+        echo "<img src='" . $targetDir . $image["file"] . "' alt='" . $image["description"] . "'>";
+
+        // display the image description
+        echo "<p>" . $image["description"] . "</p>";
+
+        // close the image-container div
+        echo "</div>";
+    }
+    ?>
+
+    <!-- add image upload form -->
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+        <label for="file">Choose image:</label>
+        <input type="file" name="file" id="file" required>
+        <label for="description">Image description:</label>
+        <textarea name="description" id="description" required></textarea>
+        <button type="submit">Upload</button>
+    </form>
 </section>
 
-<?php
-include "footer.php";
-?>
+<style>
+    .image-container {
+        float: left;
+        width: 50%;
+        box-sizing: border-box;
+        padding: 10px;
+        text-align: center;
+    }
+
+    .image-container img {
+        max-width: 100%;
+        height: auto;
+    }
+</style>
