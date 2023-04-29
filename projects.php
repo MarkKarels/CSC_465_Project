20425aa3245680d13963d58ff0b16105e11de52f
Mark Karels <mak8966@uncw.edu>
2023-04-29 18:20:18 -0400

    testing page

--- projects.php
+++ projects.php
@@ -8,14 +8,31 @@ require("includes/header.php");
 <section>
     <h1>Projects</h1>
     <p>
-        Here you can find a list of all the coding projects I have worked on.
-        You can view the code and even test it out in various languages
-        including Python, Java, Dart, C++, C#, Swift, HTML, CSS, JavaScript,
-        PHP, etc.
+        Here you can test code to your hearts content using the following IDEs
     </p>
-    <!-- add code snippets and testing functionality here -->
+    <div class="row">
+        <div class="column">
+            <h2>PYTHON</h2>
+            <iframe height="500px" width="100%" src="https://repl.it/@MarkKarels/PythonIDE?lite=true"></iframe>
+        </div>
+        <div class="column">
+            <h2>JAVA</h2>
+            <iframe height="500px" width="100%" src="https://repl.it/@MarkKarels/JavaIDE?lite=true"></iframe>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <h2>HTML, CSS, and JAVASCRIPT</h2>
+            <iframe height="500px" width="100%" src="https://repl.it/@MarkKarels/FrontEnd?lite=true"></iframe>
+        </div>
+        <div class="column">
+            <h2>C++</h2>
+            <iframe height="500px" width="100%" src="https://repl.it/@MarkKarels/CIDE?lite=true"></iframe>
+        </div>
+    </div>
 </section>
 
 <?php
 include "includes/footer.php";
+echo '<link rel="stylesheet" type="text/css" href="styles/testing.css">';
 ?>
